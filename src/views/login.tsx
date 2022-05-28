446d1b7fd4bc52aad989789b92bb51c2316a8c03
Xavier Augusto García Fernández <xgarcia-2020012@kinal.edu.gt>
2022-05-28 14:08:42 -0600

    Cambio de nombre
    nombre

--- src/views/login.tsx
+++ src/views/login.tsx
@@ -16,7 +16,7 @@ const LogIn = () => {
       <View>
         <Text style={styles.topText}>Identity</Text>
         <Text style={styles.subText}>
-          Enter a good profile photo and type your name xavier garcia 
+          Enter a good profile photo and type your name xavie dernandez
         </Text>
         <View style={styles.container2}>
           <View style={styles.logo}>
